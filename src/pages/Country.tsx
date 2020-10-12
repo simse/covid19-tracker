bf96e9730ad5f23d3a5839ab67a5bd3674363b10
Simon Sorensen <hello@simse.io>
2020-10-12 03:47:33 +0200

    first working version

--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { match } from "react-router-dom";
-import { IonContent, IonHeader, IonPage, IonToolbar, IonButtons, IonBackButton, IonIcon } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonToolbar, IonButtons, IonBackButton, IonIcon, IonSegment, IonSegmentButton, IonLabel } from '@ionic/react';
 import { arrowUpOutline, arrowDownOutline } from 'ionicons/icons';
 import "../styles/country.scss";
-//import { RouteComponentProps } from 'react-router';
+import Chart from 'chart.js';
 
 interface CountryParams {
   id: string;
 }
 
-interface IProps  {
+interface IProps {
   required: string;
   match?: match<CountryParams>
 }
@@ -17,6 +17,12 @@ interface IProps  {
 interface IState {
   countryIso: string;
   country: CountryData;
+  view: string;
+}
+
+interface CountryParent {
+  name?: string;
+  formal_name?: string;
 }
 
 interface CountryData {
@@ -29,24 +35,43 @@ interface CountryData {
   last_7_days_cases_trend?: number;
   last_7_days_deaths_trend?: number;
   graph_beginning?: string;
-  cases_graph?: Array<number>;
-  deaths_graph?: Array<number>;
-  country_name?: string;
-  country_code?: string;
+  graph_cases?: Array<number>;
+  graph_deaths?: Array<number>;
+  name?: string;
+  formal_name?: string;
+  parent?: CountryParent;
+  iso?: string;
+}
+
+interface GraphPoint {
+  x: number;
+  y: number;
 }
 
 class Country extends React.Component<IProps, IState> {
+  private allCasesGraph: React.RefObject<HTMLCanvasElement>;
+  private allDeathsGraph: React.RefObject<HTMLCanvasElement>;
+  private pastMonthCasesGraph: React.RefObject<HTMLCanvasElement>;
+  private pastMonthDeathsGraph: React.RefObject<HTMLCanvasElement>;
+
+
   constructor(props: IProps) {
     super(props)
 
     if (props) {
       if (props.match) {
         this.state = {
+          view: "statistics",
           countryIso: props.match.params.id,
           country: {}
         }
       }
     }
+
+    this.allCasesGraph = React.createRef();
+    this.allDeathsGraph = React.createRef();
+    this.pastMonthCasesGraph = React.createRef();
+    this.pastMonthDeathsGraph = React.createRef();
   }
 
   componentDidMount() {
@@ -59,6 +84,8 @@ class Country extends React.Component<IProps, IState> {
           this.setState({
             country: result
           })
+
+          this.drawAllGraphs()
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -67,6 +94,125 @@ class Country extends React.Component<IProps, IState> {
           //this.setLoadingState(false)
         }
       )
+
+    Chart.defaults.global.defaultFontFamily = '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+  }
+
+  drawGraph(ref: React.RefObject<HTMLCanvasElement>, color: string, data?: Array<GraphPoint>) {
+    if (ref.current) {
+      new Chart(ref.current, {
+        type: 'scatter',
+        data: {
+          labels: [],
+          datasets: [{
+            showLine: true,
+            data: data,
+            borderWidth: 2,
+            borderColor: color,
+            pointRadius: 0
+          }]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          tooltips: {
+            enabled: false
+          },
+          scales: {
+            xAxes: [{
+              ticks: {
+                display: false
+              }
+            }]
+          }
+        }
+      });
+    }
+  }
+
+  drawAllGraphs() {
+    this.drawGraph(this.allCasesGraph, "#3880ff", this.getTotalCasesGraph())
+    this.drawGraph(this.allDeathsGraph, "#eb445a", this.getTotalDeathsGraph())
+    this.drawGraph(this.pastMonthCasesGraph, "#3880ff", this.getCasesPastMonthGraph())
+    this.drawGraph(this.pastMonthDeathsGraph, "#eb445a", this.getDeathsPastMonthGraph())
+  }
+
+  generateGraphDataset(data: number[]) {
+    let i = 1;
+    let graph = Array<GraphPoint>();
+
+    data.forEach(element => {
+      if (element === 0) return;
+
+      graph.push({
+        x: i,
+        y: element
+      })
+
+      i++;
+    });
+
+    return graph;
+  }
+
+  intervalDataSet(dataset: number[], interval: number) {
+    let finalDataSet: number[] = []
+    let filteredDataSet = dataset.filter(point => {
+      return point > 0
+    })
+
+    let i = 1
+
+    filteredDataSet.forEach(point => {
+      if (i % interval === 0) {
+        finalDataSet.push(point)
+      }
+
+      i++
+    })
+
+    return finalDataSet
+  }
+
+  getTotalCasesGraph() {
+    if(this.state.country.graph_cases) {
+      return this.generateGraphDataset(this.intervalDataSet(this.state.country.graph_cases, 5))
+    }
+  }
+
+  getTotalDeathsGraph() {
+    if(this.state.country.graph_deaths) {
+      return this.generateGraphDataset(this.intervalDataSet(this.state.country.graph_deaths, 5))
+    }
+  }
+
+  getCasesPastMonthGraph() {
+    if(this.state.country.graph_cases) {
+      let pastMonth = this.state.country.graph_cases.slice(Math.max(this.state.country.graph_cases.length - 30, 0))
+
+      return this.generateGraphDataset(pastMonth)
+    }
+  }
+
+  getDeathsPastMonthGraph() {
+    if(this.state.country.graph_deaths) {
+      let pastMonth = this.state.country.graph_deaths.slice(Math.max(this.state.country.graph_deaths.length - 30, 0))
+
+      return this.generateGraphDataset(pastMonth)
+    }
+  }
+
+  setView(view?: string) {
+    if (view) {
+      this.setState({
+        view: view
+      })
+
+      if (view === "graphs") {
+        this.drawAllGraphs()
+      }
+    }
   }
 
   getCountryFlag(iso?: string) {
@@ -84,13 +230,13 @@ class Country extends React.Component<IProps, IState> {
       if (trend > 0) {
         return (
           <div className={"trend bad"}>
-            <IonIcon icon={arrowUpOutline} /> <span>+{ trend }%</span>
+            <IonIcon icon={arrowUpOutline} /> <span>+{trend}%</span>
           </div>
         )
       } else {
         return (
           <div className={"trend good"}>
-            <IonIcon icon={arrowDownOutline} /> <span>{ trend }%</span>
+            <IonIcon icon={arrowDownOutline} /> <span>{trend}%</span>
           </div>
         )
       }
@@ -105,6 +251,14 @@ class Country extends React.Component<IProps, IState> {
     }
   }
 
+  showParent() {
+    if (this.state.country.parent?.formal_name) {
+      return (
+        <span className="parent">Part of {this.state.country.parent.formal_name}</span>
+      )
+    }
+  }
+
   render() {
     return (
       <IonPage className={"country"}>
@@ -116,53 +270,81 @@ class Country extends React.Component<IProps, IState> {
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
-  
+
 
           <div className={"header"}>
-            <img 
-              src={this.getCountryFlag(this.state.country.country_code)}
+            <img
+              src={this.getCountryFlag(this.state.country.iso)}
               className={"flag"}
-              alt={"The flag of " + this.state.country.country_name} 
+              alt={"The flag of " + this.state.country.name}
             />
 
-            <h2>{ this.state.country.country_name }</h2>
+            <h2>{this.state.country.name}</h2>
+            {this.showParent()}
           </div>
 
-          <h3 className={"section-title"}>Total statistics</h3>
-          <div className={"stats"}>
-            <div className={"cases"}>
-
-              {this.formatTrend(this.state.country.last_7_days_cases_trend)}
-              <h1>{ this.formatNumber(this.state.country.total_cases) }</h1>
-              <span>Total COVID-19 cases</span>
-
-            </div>
-
-            <div className={"deaths"}>
-
-              {this.formatTrend(this.state.country.last_7_days_deaths_trend)}
-              <h1>{ this.formatNumber(this.state.country.total_deaths) }</h1>
-              <span>Total COVID-19 related deaths</span>
-
-            </div>
+          <div className={"selector"}>
+            <IonSegment onIonChange={e => this.setView(e.detail.value)} value={this.state.view}>
+              <IonSegmentButton value="statistics">
+                <IonLabel>Statistics</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton value="graphs">
+                <IonLabel>Graphs</IonLabel>
+              </IonSegmentButton>
+            </IonSegment>
           </div>
 
-          <h3 className={"section-title"}>Last 7 days</h3>
-          <div className={"stats"}>
-            <div className={"cases"}>
+          {this.state.view === "statistics" && <div className={"view statistics"}>
+            <h3 className={"section-title"}>Total statistics</h3>
+            <div className={"stats"}>
+              <div className={"cases"}>
 
-              <h1>{ this.formatNumber(this.state.country.last_7_days_cases) }</h1>
-              <span>COVID-19 cases in the past week</span>
+                {this.formatTrend(this.state.country.last_7_days_cases_trend)}
+                <h1>{this.formatNumber(this.state.country.total_cases)}</h1>
+                <span>Total COVID-19 cases</span>
 
+              </div>
+
+              <div className={"deaths"}>
+
+                {this.formatTrend(this.state.country.last_7_days_deaths_trend)}
+                <h1>{this.formatNumber(this.state.country.total_deaths)}</h1>
+                <span>Total COVID-19 related deaths</span>
+
+              </div>
             </div>
 
-            <div className={"deaths"}>
+            <h3 className={"section-title"}>Last 7 days</h3>
+            <div className={"stats"}>
+              <div className={"cases"}>
 
-              <h1>{ this.formatNumber(this.state.country.last_7_days_deaths) }</h1>
-              <span>COVID-19 related deaths in the past week</span>
+                <h1>{this.formatNumber(this.state.country.last_7_days_cases)}</h1>
+                <span>COVID-19 cases in the past week</span>
 
+              </div>
+
+              <div className={"deaths"}>
+
+                <h1>{this.formatNumber(this.state.country.last_7_days_deaths)}</h1>
+                <span>COVID-19 related deaths in the past week</span>
+
+              </div>
             </div>
-          </div>
+          </div>}
+
+          {this.state.view === "graphs" && <div className={"view graphs"}>
+            <h3 className={"section-title"}>All cases</h3>
+            <canvas ref={this.allCasesGraph} />
+
+            <h3 className={"section-title"}>All COVID-19 related deaths</h3>
+            <canvas ref={this.allDeathsGraph} />
+
+            <h3 className={"section-title"}>Cases past 30 days</h3>
+            <canvas ref={this.pastMonthCasesGraph} />
+
+            <h3 className={"section-title"}>COVID-19 related deaths past 30 days</h3>
+            <canvas ref={this.pastMonthDeathsGraph} />
+          </div>}
         </IonContent>
       </IonPage>
     );
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,46 +1,110 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonChip, IonLabel, IonIcon } from '@ionic/react';
-import { arrowUpOutline } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon } from '@ionic/react';
+import { arrowUpOutline, arrowDownOutline } from 'ionicons/icons';
 import './Tab1.scss';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage className={"dashboard"}>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Dashboard</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+interface IProps {}
+
+interface IState {
+  globalCases: number;
+  globalDeaths: number;
+  globalCaseTrend: number;
+  globalDeathTrend: number;
+}
+
+class Tab1 extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+
+    this.state = {
+      globalCases: 0,
+      globalDeaths: 0,
+      globalCaseTrend: 0,
+      globalDeathTrend: 0
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.avoidthe.icu/worldwide.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            globalCases: result.global_cases,
+            globalDeaths: result.global_deaths,
+            globalCaseTrend: result.global_case_trend,
+            globalDeathTrend: result.global_death_trend
+          })
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        () => {
+          //this.setLoadingState(false)
+        }
+      )
+  }
+
+  formatTrend(trend?: number) {
+    if (trend) {
+      trend = Math.round(trend * 100) / 100
+
+      if (trend > 0) {
+        return (
+          <div className={"trend bad"}>
+            <IonIcon icon={arrowUpOutline} /> <span>+{trend}%</span>
+          </div>
+        )
+      } else {
+        return (
+          <div className={"trend good"}>
+            <IonIcon icon={arrowDownOutline} /> <span>{trend}%</span>
+          </div>
+        )
+      }
+    }
+  }
+
+  formatNumber(num: number) {
+    return Math.round(num / 10000) / 100 + "M"
+  }
+
+  render() {
+    return (
+      <IonPage className={"dashboard"}>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Dashboard</IonTitle>
+            <IonTitle>Dashboard</IonTitle>
           </IonToolbar>
         </IonHeader>
-        
-        <div className={"padding"}>
-          <h3 className={"section-title"}>Worldwide statistics</h3>
-
-          <IonChip outline={true}>
-            <IonLabel color="default" >Cases</IonLabel>
-          </IonChip>
-
-          <IonChip>
-            <IonLabel color="danger">Deaths</IonLabel>
-          </IonChip>
-
-          <div className={"stats"}>
-            <div className={"trend"}>
-              <IonIcon icon={arrowUpOutline} /> <span>+4.2%</span>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Dashboard</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          
+          <div className={"padding"}>
+            <h3 className={"section-title"}>Worldwide statistics</h3>
+  
+            <div className={"stats"}>
+              {this.formatTrend(this.state.globalCaseTrend)}
+  
+              <h1>{ this.formatNumber(this.state.globalCases) }</h1>
+              <span>confirmed cases of COVID-19</span>
             </div>
-
-            <h1>1.07M</h1>
-            <span>deaths related to COVID-19</span>
+  
+            <div className={"stats"}>
+            {this.formatTrend(this.state.globalDeathTrend)}
+  
+              <h1>{ this.formatNumber(this.state.globalDeaths) }</h1>
+              <span>deaths related to COVID-19</span>
+            </div>
           </div>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
 
 export default Tab1;
